4a620b192fc565f190a88ef0be02309d7084d94a
Nelson P <141482619+nelson160999@users.noreply.github.com>
2023-12-07 18:34:27 +0530

    Edited the code

--- app.js
+++ app.js
@@ -3,32 +3,23 @@ const https=require('https')
 const fs=require('fs')
 var bodyParser = require('body-parser')
 var app = express();
+const PORT = process.env.PORT || 3030;
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
 app.get("/", function (request, response) {
-  response.send('Simple WhatsApp Webhook tester</br>There is no front-end, see server.js for implementation!');
+  response.send('Simple Webhook tester</br>There is no front-end, see server.js for implementation!');
 });
 
-app.get('/webhook', function(req, res) {
-  console.log(req.query)
-  if (
-    req.query['hub.mode'] == 'subscribe' &&
-    req.query['hub.verify_token'] == 'token'
-  ) {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.sendStatus(200);
-  }
-});
 
 app.post("/webhook", function (request, response) {
   console.log('Incoming webhook: ' + JSON.stringify(request.body));
   response.sendStatus(200);
 });
 
-var listener = app.listen(8001, function () {
+var listener = app.listen(PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
