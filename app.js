301602ca4672175d34a31536469595a4c5b034d4
Nelson P <141482619+nelson160999@users.noreply.github.com>
2023-12-21 13:20:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
 app.get("/", function (request, response) {
-  response.send('Simple Webhook tester</br>There is no front-end, see server.js for implementation!');
+  response.send('Comming Soon');
 });
 
 
